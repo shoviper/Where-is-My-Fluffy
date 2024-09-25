fdfc3d6f5bcfb861fc1e903d5f83323511bba091
lollipop <hastyfasd@gmai.com>
2024-09-25 14:06:01 +0700

    Add initial project files and configurations

---
+++ src/main/java/com/sda_project/myfluffy/MyfluffyApplication.java
@@ -0,0 +1,13 @@
+package com.sda_project.myfluffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfluffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfluffyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda_project/myfluffy/MyfluffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda_project.myfluffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfluffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
