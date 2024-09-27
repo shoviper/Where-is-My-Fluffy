c7abb9c504806ff182ca43cf645a0c9f201d2aa6
porfai <popeachyyy@gmail.com>
2024-09-27 13:35:21 +0700

    fix/ css Home,Login and  add/ logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,10 +17,11 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+	/* margin-top: 60px;	 */
 }
 
 button {
 	cursor: pointer;
 }
+
 </style>
