a0ea1de19f718d4f7984084351bc612dcb93ab50
shoviper <65011304@kmitl.ac.th>
2024-10-06 23:03:25 +0700

    Fix: notification

--- frontend/src/components/Notifications.vue
+++ frontend/src/components/Notifications.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <div class="flex justify-between bg-white py-6 items-center m sticky top-0">
       <h2 class="text-base font-semibold">Notifications</h2>
-      <a href="#" class="text-base text-red-500">Mark all as read</a>
+      <a href="/seeallnotification" class="text-base text-red-500">Mark all as read</a>
     </div>
 
     <!-- Notifications List -->
@@ -11,7 +11,7 @@
       
 
       <div class="border-t border-gray-300 py-2" v-for="(mesg, index) in notification" :key="index">
-        <div>
+        <div @click="goto({ path: `/seeeachnotification/${mesg.id}` })">
           <p class="text-sm text-TEXTCOLOR text-left">
             {{ mesg.message }}
           </p>
@@ -49,6 +49,16 @@ export default {
         console.error("Error fetching animal types:", error);
       }
     },
+    goto(page) {
+          if (page.name && page.name !== this.$route.name) {
+            this.$router.push({ name: page.name });
+            return;
+          }
+          if (page.path && page.path !== this.$route.path) {
+            this.$router.push({ path: page.path });
+            return;
+          }
+        },
   },
 };
 </script>
